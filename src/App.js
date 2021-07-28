c9cd4552e67322b520796b92cbf70de4bcea6340
yukyoooo <mrym12000@gmail.com>
2021-07-28 16:46:12 +0900

    react flagment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React, { Component } from "react";
 
-function App() {
-  return <h1>hello, world</h1>;
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("i am clicked")}} />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
