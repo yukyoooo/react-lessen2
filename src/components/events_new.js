41345506834b4e3a8b79563ff463c65292cc8084
yukyoooo <mrym12000@gmail.com>
2021-10-03 12:10:49 +0900

    submitボタン日活性化

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -25,13 +25,13 @@ class EventsNew extends Component {
     this.props.history.push('/')
   }
   render() {
-    const { handleSubmit } = this.props
+    const { handleSubmit, pristine, submitting } = this.props
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
         <div>
-          <input type="submit" value="Submit" disabled={false} />
+          <input type="submit" value="Submit" disabled={pristine || submitting} />
           <Link to="/" >Cancel</Link>
         </div>
       </form>
